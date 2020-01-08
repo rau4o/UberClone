b981995624ff4450fc55c53020de3d7adca6897d
rau4o <rkoshkinbayev@gmail.com>
2020-01-08 17:46:55 +0600

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by rau4o on 1/8/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
