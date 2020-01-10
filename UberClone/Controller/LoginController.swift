7ac66d7f5301d570a9fb5bcf574e0926ded7b8e6
rau4o <rkoshkinbayev@gmail.com>
2020-01-10 13:02:18 +0600

    first commit

---
+++ UberClone/Controller/LoginController.swift
@@ -0,0 +1,103 @@
+//
+//  LoginController.swift
+//  UberClone
+//
+//  Created by rau4o on 1/8/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    //MARK: Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContrainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContrainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Email",
+                                       isSecureTextEntry: false)
+    }()
+    
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Password",
+                                       isSecureTextEntry: true)
+    }()
+    
+    private let loginButton: AuthButton = {
+        let button = AuthButton(type: .system)
+        button.setTitle("Log in ", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16),NSAttributedString.Key.foregroundColor: UIColor.mainBlueTint]))
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    // MARK: LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    // MARK: - Selectors
+    
+    @objc func handleShowSignUp() {
+        let controller = SignUpController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    // MARK: - Helper functions
+    
+    func configureUI() {
+        configureNavigationBar()
+        
+        view.addSubview(titleLabel)
+        view.backgroundColor = .backgroundColor
+        
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailContainerView,passwordContainerView,loginButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 24
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: titleLabel.bottomAnchor,left: view.leftAnchor,right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16)
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,height: 32)
+    }
+    
+    func configureNavigationBar() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+    }
+}
